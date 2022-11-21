82790bb525c9b3f2d8b373ebd34e50a4d0357bdf
tienld235 <leductien1997123@gmail.com>
2022-11-21 09:27:52 +0700

    progress

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/app/data/services/storage/services.dart';
 import 'package:weather_app/app/modules/home/view.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 
 void main() async {
   await GetStorage.init();
+  await Get.putAsync(() => StorageService().init());
   runApp(const MyApp());
 }
 
